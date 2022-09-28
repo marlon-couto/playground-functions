3622f18bef864d6817037f230a9ffabacc2dd932
Marlon Couto <marloncouto199@gmail.com>
2022-09-27 21:27:10 -0300

    Inicia resolução dos requisitos bônus
    Signed-off-by: Marlon Couto <marloncouto199@gmail.com>

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,16 @@
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(array) {
   // seu código aqui
+  let phoneNumber = `(${array[0]}${array[1]}) ${array[2]}${array[3]}${array[4]}${array[5]}${array[6]}-${array[7]}${array[8]}${array[9]}${array[10]}`;
+
+  if (array.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+
+  return phoneNumber;
 }
+
+// console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
 
 // Desafio 12
 function triangleCheck() {
